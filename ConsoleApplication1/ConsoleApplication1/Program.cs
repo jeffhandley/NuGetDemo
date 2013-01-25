d0aa4dc081cddc23fc5757ce457feed8c4cf5183
Jeff Handley <jeff@jeffhandley.com>
2013-01-25 14:32:19 -0800

    Foo

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DbContext foo = new DbContext("foo");
+            Console.WriteLine(foo.Database.Connection.ConnectionString);
+        }
+    }
+}
